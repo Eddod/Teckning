c1bdba1cc31a05ae909b3de8e791129cc0635689
Eddod <edwinwesterberg44@gmail.com>
2021-11-08 19:04:02 +0100

    Add project files.

---
+++ Teckning/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Teckning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Teckning Cirkel = new Cirkel();
+            Console.WriteLine("Arean på cirkeln  : {0}",Cirkel.Area());
+
+            Triangle Test = new Triangle();
+            Console.WriteLine("Arean på triangeln  : {0}",Test.Area());
+
+            Rectangle R1 = new Rectangle();
+            Console.WriteLine("Arean på rektangeln  : {0}", R1.Area()); 
+
+
+        }
+    }
+}
---
+++ Teckning/Teckning.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Teckning
+{
+    public class Teckning
+    {
+        public virtual double Area()
+        {
+            return 0;
+        }
+
+    }
+    public class Cirkel : Teckning
+    {
+        public double _radius { get; set; }
+        private double _pi = 3.14;
+        public Cirkel()
+        {
+            this._radius = 4;
+        }
+        public override double Area()
+        {
+
+            return _radius * _radius * _pi;
+        }
+
+    }
+    public class Triangle : Teckning
+    {
+        double _height { get; set; }
+        double _base { get; set; }
+
+        public Triangle()
+        {
+            this._height = 10;
+            this._base = 15;
+        }
+        public override double Area()
+        {
+            return (_height * _base) / 2;
+        }
+    }
+    public class Rectangle : Teckning
+    {
+        double _height { get; set; }
+        double _base { get; set; }
+
+        public Rectangle()
+        {
+            this._height = 4;
+            this._base = 10;
+        }
+        public override double Area()
+        {
+
+            return _height * _base;
+
+        }
+    }
+}
